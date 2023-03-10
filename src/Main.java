6f0030626491e44401dd2bf488a069f0e3ea8fd6
Martin Stamm <Martin.Stamm@Eilebrecht.de>
2023-03-10 17:25:26 +0100

    styling and regex parser

--- src/Main.java
+++ src/Main.java
@@ -29,14 +29,7 @@ public class Main {
 
         //sending all messanges from the nodes to the others
         for (int i = 0; i <= maxNodes; i++) {
-            //get the messanges from every node
-            for (Node node:nodeList) {
-                List <Node.Message> messageList = node.sendOutMessages();
-                // send all messages from a node to the linked nodes
-                for (Node.Message message: messageList) {
-                    sendMessageToNode(message);
-                }
-            }
+            askEveryNodeForMessangesAndDistributeToOthers();
 
             if(SHOW_SPANNINGTREE_BUILD_UP){
                 System.out.println("Durchlauf: " +i);
@@ -51,16 +44,28 @@ public class Main {
         outputOfSpannigTree();
     }
 
-    //shows all information about the nodes and links
-    private static void showAllInformations() {
-        System.out.println("Maxnodes: " + maxNodes);
+    private static void askEveryNodeForMessangesAndDistributeToOthers() {
+        //get the messanges from every node
+        for (Node node:nodeList) {
+            List <Node.Message> messageList = node.sendOutMessages();
+            sendEveryNodeHisMessage(messageList);
+        }
+    }
 
+    private static void sendEveryNodeHisMessage(List<Node.Message> messageList) {
+        // send all messages from a node to the linked nodes
+        for (Node.Message message: messageList) {
+            sendMessageToNode(message);
+        }
+    }
+
+
+    //forward the incoming messages to the nodes
+    private static void sendMessageToNode(Node.Message message) {
         for (Node node: nodeList) {
-            System.out.println(node.getName() + " = " + node.getNodeID() + " Root: " + node.getRoot()  + ", next hop: " + node.getNextHop() + ", kosten zum root: " + node.getSummeKosten());
-
-            List <Link> linkList = node.getLinkList();
-            for (Link link: linkList) {
-                System.out.println(link.startknoten + " -> " + link.zielknoten + ": "+ link.getSummeKosten());
+            //send messae to the endnode
+            if(node.getName().equals(message.zielNode())){
+                node.inputMessage(message);
             }
         }
     }
@@ -81,16 +86,6 @@ public class Main {
         }
     }
 
-    //forward the incoming messages to the nodes
-    private static void sendMessageToNode(Node.Message message) {
-        for (Node node: nodeList) {
-            //send messae to the endnode
-            if(node.getName().equals(message.zielNode())){
-                node.inputMessage(message);
-            }
-        }
-    }
-
     //reads in the data from the file into the objects
     private static int readInFile(String pathToFile) {
         int maxNodes = 0;
@@ -106,22 +101,17 @@ public class Main {
             //Read untile end of file
             while (reader.hasNextLine()) {
                 String line = reader.nextLine();
-
-                //todo: regex parser?
-                    //check if line is valid
+                
+                //check if line is valid
                 if(lineIsInvalid(line)) {
 
                 //read in Nodes with form A = 5;
                 } else if(Pattern.matches("^ *[a-zA-Z]+[ ]*=[ ]*[\\d]+[ ]*;$", line)){
-
-                    //read in nodes
-                //} else if(line.contains("=")){
                     nodeList.add(new Node(line));
                     maxNodes++;
 
-                //read in links
-                } else if (line.contains("-")) {
-                    //linkList.add(new Link(line));
+                //read in links with form  B - E : 10;
+                } else if (Pattern.matches("^ *[a-zA-Z]+[ ]*-[ ]*[a-zA-Z]+[ ]*:[ ]*[\\d]+[ ]*;$", line)) {
                     addLinkToNodes(new Link(line));
 
                 } else {
@@ -159,5 +149,20 @@ public class Main {
     //check if line is a comment or a start or endframe
     private static boolean lineIsInvalid(String line) {
         return (line.contains("/") || line.contains("{") || line.contains("}"));
+    }
+
+
+    //shows all information about the nodes and links
+    private static void showAllInformations() {
+        System.out.println("Maxnodes: " + maxNodes);
+
+        for (Node node: nodeList) {
+            System.out.println(node.getName() + " = " + node.getNodeID() + " Root: " + node.getRoot()  + ", next hop: " + node.getNextHop() + ", kosten zum root: " + node.getSummeKosten());
+
+            List <Link> linkList = node.getLinkList();
+            for (Link link: linkList) {
+                System.out.println(link.startknoten + " -> " + link.zielknoten + ": "+ link.getSummeKosten());
+            }
+        }
     }
 }
